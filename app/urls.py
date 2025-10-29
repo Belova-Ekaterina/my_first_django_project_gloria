e2150dacf395099e0dcd9fc5390fa67cd4f9af32
Belova-Ekaterina <belova-eks@yandex.ru>
2025-10-30 00:32:20 +0300

    главная страница с товарами

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
+
 from app import views
 
-urlpatterns = [
-    path("main-page/", views.index, name="index")
-]
+urlpatterns = [path("main-page/", views.index, name="index")]
--- app/views.py
+++ app/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import CategoryForm
 from .models import Brand, Category, Product
 
-def index(request):
 
+def index(request):
     products = Product.objects.all()
     brands = Brand.objects.all()
     categories = Category.objects.all()
+
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = CategoryForm()
 
     context = {
         "products": products,
         "brands": brands,
         "categories": categories,
+        "form": form,
     }
-
-    return render(request, "app/index.html", context=context)
+    return render(request, "app/index.html", context)
